c9b4dbe4a86c8586ea3bf125bf51bc237de11553
mathpvx <7547@holbertonstudents.com>
2025-01-30 17:15:36 +0100

    dyanmic tutorial videos

--- script.js
+++ script.js
@@ -109,3 +109,70 @@ $(document).ready(function () {
     
     fetchQuotes();
 });
+
+$(document).ready(function () {
+    const QUOTES_API_URL = "https://smileschool-api.hbtn.info/quotes";
+    const VIDEOS_API_URL = "https://smileschool-api.hbtn.info/popular-tutorials";
+    
+    function fetchQuotes() {
+        $(".carousel-inner").html('<div class="text-center w-100 py-5"><img src="images/loader.gif" alt="Loading..." /></div>');
+        
+        $.get(QUOTES_API_URL, function (data) {
+            $(".carousel-inner").empty();
+            
+            data.forEach((quote, index) => {
+                let activeClass = index === 0 ? "active" : "";
+                let quoteItem = `
+                    <div class="carousel-item ${activeClass}">
+                        <div class="row mx-auto align-items-center">
+                            <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
+                                <img src="${quote.pic_url}" class="d-block align-self-center rounded-circle" alt="${quote.name}">
+                            </div>
+                            <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
+                                <div class="quote-text">
+                                    <p class="text-white">« ${quote.text} »</p>
+                                    <h4 class="text-white font-weight-bold">${quote.name}</h4>
+                                    <span class="text-white">${quote.title}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+                
+                $(".carousel-inner").append(quoteItem);
+            });
+        });
+    }
+    
+    function fetchVideos() {
+        $(".videos-carousel").html('<div class="text-center w-100 py-5"><img src="images/loader.gif" alt="Loading..." /></div>');
+        
+        $.get(VIDEOS_API_URL, function (data) {
+            $(".videos-carousel").empty();
+            
+            data.forEach(video => {
+                let videoCard = `
+                    <div class="carousel-item">
+                        <div class="card">
+                            <img src="${video.thumb_url}" class="card-img-top" alt="${video.title}">
+                            <div class="card-body">
+                                <h5 class="card-title font-weight-bold">${video.title}</h5>
+                                <p class="card-text text-muted">${video.sub_title}</p>
+                                <div class="creator d-flex align-items-center">
+                                    <img src="${video.author_pic_url}" alt="${video.author}" width="30px" class="rounded-circle">
+                                    <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                                </div>
+                                <div class="info pt-3 d-flex justify-content-between">
+                                    <span class="main-color">${video.duration}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+                
+                $(".videos-carousel").append(videoCard);
+            });
+        });
+    }
+    
+    fetchQuotes();
+    fetchVideos();
+});
